24ed85dc369ea5047b2d0fef4a9560bcddf0c480
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 11:25:00 +0300

    in proc

---
+++ DZ_2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = 3, b = 7;
+
+            for (uint i = a; i <= b; ++i)
+            {
+                for (uint j = 0; j < i; ++j)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
