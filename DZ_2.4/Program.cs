07017625e928a07d85d1c6e776357fa89e1967cc
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 11:26:16 +0300

    DONE

---
+++ DZ_2.4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 12345;
+
+            string line2 = string.Empty;
+            string line3 = string.Empty;
+            string line4 = string.Empty;
+            string line5 = string.Empty;
+
+            string line1 = n.ToString();
+            string temp;
+            int len = line1.Length;
+            for (int i = 0; i < len; ++i)
+            {
+                temp = line1.Substring(len - 1 - i, 1);
+
+                line2 = line2 + temp;
+                line3 = string.Concat(line3, temp);
+                line4 = string.Format("{0}{1}", line4, temp);
+                line5 = new StringBuilder(line5).Append(temp).ToString();
+            }
+
+            Console.Write("\n\n\nn = " + line1 + "   -   " + line2 + " " + line3 + " " + line4 + " " + line5);
+
+            int N = int.Parse(line5);
+
+            Console.WriteLine("\n\n\nN = " + N);
+            Console.ReadKey();
+        }
+    }
+}
